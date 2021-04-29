2989ba397eee88f7ee1d1602c4be4f916bc7885c
James Whiteley <james.al.whiteley@gmail.com>
2021-04-29 11:10:15 +0100

    disabeled NU1701 warming about .NETFramework compatibility in BeaverCore.csproj set GenerateProgramFile to false in BeaverTest.csproj so "static void Main" can be used in RunTests.cs

---
+++ BeaverTest/RunTests.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace BeaverTest
+    /// <summary>
+    /// Script used for display output to terminal (for debugging) 
+    /// set GenerateProgramFile to false in .csproj to use this script (via "static void Main")
+    /// otherwise comment out "static void Main" and use dotnet test to call tests 
+    /// </summary>
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ BeaverTest/TestULS.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using BeaverCore.Frame;
+using BeaverCore.Actions;
+using System;
+
+namespace BeaverTest
+{
+    public class TestULS
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestULSDesign()
+        {
+            Force Forces = new Force();
+            // new TimberFramePoint(1);
+            Console.WriteLine("Hello World");
+
+            Assert.Pass();
+        }
+    }
+}
+
