dd930ea989e1864138b9817b35196c1539fe4613
James Whiteley <james.al.whiteley@gmail.com>
2021-04-29 21:11:07 +0100

    added RunTests.cs, description in file TestULS.cs - WIP, tests example ULS calculation using TimberFramePoint

--- BeaverTest/TestULS.cs
+++ BeaverTest/TestULS.cs
@@ -1,10 +1,17 @@
+using System;
 using NUnit.Framework;
+using System.Collections.Generic;
 using BeaverCore.Frame;
 using BeaverCore.Actions;
-using System;
+using BeaverCore.CrossSection;
+using BeaverCore.Materials;
 
 namespace BeaverTest
 {
+    /// <summary>
+    /// WIP. Tests example ULS calculation using TimberFramePoint 
+    /// To Do:  - displacement? 
+    /// </summary>
     public class TestULS
     {
         [SetUp]
@@ -15,11 +22,26 @@ namespace BeaverTest
         [Test]
         public void TestULSDesign()
         {
-            Force Forces = new Force();
-            // new TimberFramePoint(1);
-            Console.WriteLine("Hello World");
+            Displacement disp = new Displacement(1, "QA");
+            List<Displacement> disps = new List<Displacement>() { disp };
+            Force force = new Force(28, 3, 2, 0.2, 1.5, 0.4, "QA");
+            List<Force> forces = new List<Force>() { force };
 
-            Assert.Pass();
+            CroSec crosec = new CroSec_Rect(15, 15);
+            Material mat = new Material("C18", "Solid", 1.8, 1, 0.04, 1.8, 0.22, 0.34, 900, 600, 56, 30); // kN/cm^2, EN 338:2016
+            crosec.Mat = mat;
+
+            TimberFramePoint element = new TimberFramePoint(forces, disps, crosec, 1, 205, 205, 205, 0.7);
+            TimberFrameULSResult result = element.ULSUtilization();
+
+            /// extract results 
+            List<double[]> UtilsY = result.UtilsY;
+
+            double Util0 = UtilsY[0][0];
+            double Util1 = UtilsY[1][0];
+            Console.WriteLine(Util0);
+            Console.WriteLine(Util1);
+            
         }
     }
 }
