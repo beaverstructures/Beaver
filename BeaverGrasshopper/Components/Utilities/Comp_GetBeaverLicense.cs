e86f8eb90482ddd093fd2bf1e3101a3c66a558aa
Renan <renan.pran@gmail.com>
2022-07-16 16:07:09 +0200

    Implement GetLicense from Cloud Zoo

---
+++ BeaverGrasshopper/Components/Utilities/Comp_GetBeaverLicense.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+using BeaverGrasshopper.Components.Utilities;
+
+namespace BeaverGrasshopper.Components.Utilities
+{
+    public class GetBeaverLicense : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the MyComponent1 class.
+        /// </summary>
+        public GetBeaverLicense()
+          : base("GetBeaverLicense", "Beaver",
+              "Calls GetLicense using CloudZoo licenses",
+              "Beaver", "License")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddBooleanParameter("Button","Button","Input a boolean button here for connecting to Cloud Zoo server and retrieving your license.", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Status", "Status", "Text value indicating whether your license is valid", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            LicenceUtilities.Instance.CallGetLicense();
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return Properties.Resources.BeaverIcon;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("E1FC2EAD-8F6D-45DA-B59A-CBEC3E91A438"); }
+        }
+    }
+}
